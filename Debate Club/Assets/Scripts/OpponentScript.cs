3e7ad1d8d297bd4f9f6d1f2f39b3bd38295cee0b
CameronDavies01 <56071558+CameronDavies01@users.noreply.github.com>
2019-10-08 14:38:56 +0100

    Add 0.03 Project

---
+++ Debate Club/Assets/Scripts/OpponentScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpponentScript : MonoBehaviour
+{
+    // Establishes the opponents health
+    public int EnemyHealth = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the opponents health reaches 0 you win
+        if (EnemyHealth == (0)) {
+            Debug.Log("Test");
+        }
+    }
+}
